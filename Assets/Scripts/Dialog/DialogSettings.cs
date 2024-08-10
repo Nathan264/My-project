0813e45447efdb8611ef220b7e2acafcaf8ed61d
Nathan <almeida.nathan92@gmail.com>
2024-08-10 17:52:55 -0300

    add custom button to inspector

--- Assets/Scripts/Dialog/DialogSettings.cs
+++ Assets/Scripts/Dialog/DialogSettings.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 [CreateAssetMenu(fileName = "DialogSettings", menuName = "Dialog/DialogSettings")]
 public class DialogSettings : ScriptableObject {
@@ -7,7 +8,6 @@ public class DialogSettings : ScriptableObject {
     public GameObject actor;
     
     [Header("Dialog")]
-    public string actorName;
     public string sentence;
     public Sprite speakerSprite;
 
@@ -16,6 +16,7 @@ public class DialogSettings : ScriptableObject {
 
 [System.Serializable]
 public class Sentences {
+    public string actorName; 
     public Sprite profile;
     public Languages sentences;
 }
@@ -25,4 +26,41 @@ public class Languages {
     public string portuguese;
     public string english;
     public string spanish;
+
+    
 }
+
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(DialogSettings))]
+    public class BuilderEditor : Editor {
+        
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        DialogSettings ds = (DialogSettings)target;
+
+        Languages l = new()
+        {
+            portuguese = ds.sentence
+        };
+
+        Sentences s = new()
+        {
+            profile = ds.speakerSprite,
+            sentences = l
+        };
+
+        if (GUILayout.Button("Create Dialogue")) {
+            if (ds.sentence != "") {
+                ds.dialogues.Add(s);
+            
+                ds.sentence = "";
+                ds.speakerSprite = null;
+            }
+        }
+    }
+}
+
+#endif
